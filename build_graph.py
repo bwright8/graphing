e66a497ce50fa61b5664fc479341b18029e83c08
bwright8 <60297325+bwright8@users.noreply.github.com>
2021-03-13 09:17:18 -0500

    Update build_graph.py

--- build_graph.py
+++ build_graph.py
@@ -1,13 +1,11 @@
 import os
 import sys
 from io import StringIO
-from logger import logger
 from dotenv import load_dotenv
 import pandas as pd
 import psycopg2
 from psycopg2 import sql
 import psycopg2.extras as extras
-import meta_data as md
 import nameparser
 import probablepeople
 import usaddress
@@ -35,7 +33,12 @@ cursor = conn.cursor()
 
 def get_filing_nums(curs):
 
-    curs.execute("SELECT name,filing_num FROM master;")
+
+    curs.execute("SELECT name,filing_num FROM master LIMIT 50000;")
+    #for testing purposes
+
+    # curs.execute("SELECT name,filing_num FROM master;")
+
 
     filing_num_dict = {}
 
@@ -48,7 +51,7 @@ def get_charter_officer_data(curs):
         curs.execute("SELECT master.filing_num,name,business_name FROM master INNER JOIN charter_officer_business ON master.filing_num = charter_officer_business.filing_num;")
         return(curs.fetchall())
 
-class COB_Graph_Driver:
+class Graph_Driver:
 
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -56,10 +59,6 @@ class COB_Graph_Driver:
     def close(self):
         self.driver.close()
 
-    def print_greeting(self, message):
-        with self.driver.session() as session:
-            greeting = session.write_transaction(self._create_and_return_greeting, message)
-            print(greeting)
 
 
     @staticmethod
@@ -73,80 +72,49 @@ class COB_Graph_Driver:
 
 
     @staticmethod
-    def _create_and_return_cob_relation(tx,business_name,charter_officer_business_name):
+    def _create_and_return_cob_relation(tx,fn1,fn2):
         result = tx.run("MATCH"
-                        "   (a:business)"
-                        "   (b:business)"
+                        "   (a:Business), "
+                        "   (b:Business) "
 
-                        "WHERE a.name = $business_name AND b.name = $charter_officer_business_name"
+                        "WHERE a.filing_num = $fn1 AND b.filing_num = $fn2 "
                         "CREATE (b)-[r:is_charter_officer_business]->(a) "
-                        "RETURN type(r)", charter_officer_business_name = charter_officer_business_name, business_name = business_name)
+                        "RETURN type(r)", fn1 = fn1, fn2 = fn2)
 
-        return result.single()[0]
+        return result.single()
 
     def create_all_nodes(self,cob_node_data_dictionary):
         with self.driver.session() as session:
             i = 0
             for key in cob_node_data_dictionary.keys():
-                result = session.write_transaction(self._create_and_return_business,key,cob_node_data_dictionary[key])
+                result = session.write_transaction(self._create_and_return_business,cob_node_data_dictionary[key],key)
                 #print(result)
                 i +=1 
                 if(i % 1000000 ==0):
                     print(i)
-               
+            result = session.write_transaction(self._create_filing_number_index)
+            print(result)
             
-    def create_all_cob_edges(self,cob_data):
+    def create_all_cob_edges(self,cob_data,filing_num_dict):
         with self.driver.session() as session:
             i = 0
             for entry in cob_data:
-                result = session.write_transaction(self._create_and_return_cob_relation,cob_data[1],cob_data[2])
-                #print(result)
+                try:
+                    thing1 = filing_num_dict[entry[1]]
+                    thing2 = filing_num_dict[entry[2]]
+
+                    result = session.write_transaction(self._create_and_return_cob_relation,thing1,thing2)
+                    #print(result)
+                   
+                except:
+                   pass
+
                 i += 1
-                if(i % 10000 ==0):
-                    print(i)
+                if(i % 100000 ==0):
+                    print(i) 
 
 
 
-    @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
-        return result.single()[0]
-
-    def create_friendship(self, person1_name, person2_name):
-        with self.driver.session() as session:
-            # Write transactions allow the driver to handle retries and transient errors
-            result = session.write_transaction(
-                self._create_and_return_friendship, person1_name, person2_name)
-            for record in result:
-                print("Created friendship between: {p1}, {p2}".format(
-                    p1=record['p1'], p2=record['p2']))
-
-    @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
-
-        # To learn more about the Cypher syntax,
-        # see https://neo4j.com/docs/cypher-manual/current/
-
-        # The Reference Card is also a good resource for keywords,
-        # see https://neo4j.com/docs/cypher-refcard/current/
-
-        query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:KNOWS]->(p2) "
-            "RETURN p1, p2"
-        )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
-        try:
-            return [{"p1": record["p1"]["name"], "p2": record["p2"]["name"]}
-                    for record in result]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
 
     def find_business(self, business_name):
         with self.driver.session() as session:
@@ -155,9 +123,16 @@ class COB_Graph_Driver:
                 print("Found business: {record}".format(record=record))
 
     @staticmethod
+    def _create_filing_number_index(tx):
+        query = ("CREATE INDEX fn_index IF NOT EXISTS FOR (n:Business) ON (n.filing_num)")
+        result = tx.run(query)
+        return(result)
+
+
+    @staticmethod
     def _find_and_return_business(tx, business_name):
         query = (
-            "MATCH (b:business) "
+            "MATCH (b:Business) "
             "WHERE b.name = $business_name "
             "RETURN b.name AS name"
         )
@@ -166,10 +141,9 @@ class COB_Graph_Driver:
 
 
 if __name__ == "__main__":
-    greeter = COB_Graph_Driver("bolt://localhost:7687", "neo4j", graph_db_psswrd)
-    filing_num_dict = get_filing_nums(cursor)
+    graph_driver = Graph_Driver("bolt://localhost:7687", "neo4j", graph_db_psswrd)
+    master_filing_num_dict = get_filing_nums(cursor)
     cob_relations = get_charter_officer_data(cursor)
-    #greeter.print_greeting("hello, world")
-    #greeter.create_all_nodes(filing_num_dict)
-    greeter.create_all_cob_edges(cob_relations)
-    greeter.close()
+    graph_driver.create_all_nodes(master_filing_num_dict)
+    graph_driver.create_all_cob_edges(cob_relations, master_filing_num_dict)
+    graph_driver.close()
